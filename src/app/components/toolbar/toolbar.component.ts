2436d75985cc1e917203b9c64f1007873f4eb87e
Luiggi Yantas Inocente <luiggiyantas@gmail.com>
2022-11-03 16:20:34 -0500

    table, filter and search

---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { PeopleTableComponent } from '../people-table';
+
+@Component({
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatDialogModule],
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss'],
+})
+export class ToolbarComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
+  openDialog(
+    enterAnimationDuration: string,
+    exitAnimationDuration: string
+  ): void {
+    this.dialog.open(PeopleTableComponent, {
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+  ngOnInit(): void {}
+}
